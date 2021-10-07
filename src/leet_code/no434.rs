3c88995e346daaaa8232e821c991776bb597fc05
BKDOnions <zqdbyct@gmail.com>
2021-10-07 17:57:06 +0800

    no434 version 2

--- src/leet_code/no434.rs
+++ src/leet_code/no434.rs
@@ -2,13 +2,6 @@ use crate::algorithms::solution::Solution;
 
 impl Solution {
     pub fn count_segments(s: String) -> i32 {
-        let mut counter = 0;
-        let strings: Vec<&str> = s.split(" ").collect();
-        for string in strings {
-            if !"".eq(string) {
-                counter += 1;
-            }
-        }
-        return counter;
+        s.split_whitespace().collect::<Vec<_>>().len() as i32
     }
 }
