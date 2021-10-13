311c16ca26dc04c0577c833d399562598ab2cd91
BKDOnions <zqdbyct@gmail.com>
2021-10-13 15:26:38 +0800

    Better no412

--- src/leet_code/no412.rs
+++ src/leet_code/no412.rs
@@ -4,11 +4,16 @@ impl Solution {
     pub fn fizz_buzz(n: i32) -> Vec<String> {
         let mut res: Vec<String> = Vec::new();
         for i in 1..n + 1 {
-            match (i % 3, i % 5) {
-                (0, 0) => res.push(String::from("FizzBuzz")),
-                (0, _) => res.push(String::from("Fizz")),
-                (_, 0) => res.push(String::from("Buzz")),
-                _ => res.push(format!("{}", i)),
+            let a = i % 3;
+            let b = i % 5;
+            if a == 0 && b == 0 {
+                res.push("FizzBuzz".to_string());
+            } else if a == 0 {
+                res.push("Fizz".to_string());
+            } else if b == 0 {
+                res.push("Buzz".to_string());
+            } else {
+                res.push(i.to_string());
             }
         }
         res
