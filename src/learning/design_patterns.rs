dff3b84ce5e2350dd960012a1e49ff958e2db166
BKDOnions <zqdbyct@gmail.com>
2021-10-31 16:23:52 +0800

    Finished rust pattern

--- src/learning/design_patterns.rs
+++ src/learning/design_patterns.rs
@@ -424,6 +424,33 @@ mod factory_method {
     }
 }
 
+/// ## Singleton
+///
+/// ### Intent
+///
+/// **Singleton** is a creational design pattern that lets you ensure that a class has
+/// only one instance, while providing a global access point to this instance.
+///
+/// ### Note
+/// > The Singleton pattern solves two problems at the same time,
+/// violating the Single Responsibility Principle
+///
+/// ### Solution
+/// + Ensure that a class has just a single instance.
+/// + Provide a global access point to that instance.
+/// + Make the default constructor private,
+/// to prevent other objects from using the new operator with the Singleton class.
+/// + Create a static creation method that acts as a constructor. Under the hood,
+/// this method calls the private constructor to create an object and saves it in a static field.
+/// All following calls to this method return the cached object.
+///
+///
+///
+
+mod singleton {
+    /// Useless in rust
+}
+
 /// # Structural Patterns
 ///
 /// ## Adapter (Wrapper)
