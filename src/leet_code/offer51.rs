65ca5850ed5d97542200730def0e77c93c8c5704
Richard Tung <zqdbyct@gmail.com>
2022-03-30 17:44:12 +0800

    pending fix

--- src/leet_code/offer51.rs
+++ src/leet_code/offer51.rs
@@ -1,55 +1,59 @@
 use crate::Solution;
-use std::collections::{HashMap, HashSet};
 
 impl Solution {
     pub fn reverse_pairs(mut nums: Vec<i32>) -> i32 {
-        let mut counter: Vec<(i32, usize)> = Vec::new();
-        nums.reverse();
-        for (index, &value) in nums.iter().enumerate() {
-            let insert_index = search(&counter, value, 0, counter.len());
-            counter.insert(insert_index, (value, index));
+        if nums.len() == 0 {
+            return 0;
         }
-
-        return 0;
+        merge_sort(&mut nums)
     }
 }
 
-fn search(nums: &Vec<(i32, usize)>, target: i32, low: usize, high: usize) -> usize {
-    if high < low {
-        return low;
+pub fn merge_sort(nums: &mut Vec<i32>) -> i32 {
+    fn _merge(nums: &mut Vec<i32>, left: usize, mid: usize, right: usize) -> i32 {
+        let mut counter = 0;
+        let left_part: Vec<i32> = nums[left..mid].iter().cloned().collect();
+        let right_part: Vec<i32> = nums[mid..right].iter().cloned().collect();
+        let (mut left_offset, mut right_offset) = (0usize, 0usize);
+        while left_offset < left_part.len() || right_offset < right_part.len() {
+            if right_offset == right_part.len()
+                || (left_offset < left_part.len()
+                    && left_part[left_offset] <= right_part[right_offset])
+            {
+                nums[left + left_offset + right_offset] = left_part[left_offset];
+                left_offset += 1;
+            } else if left_offset == mid + 1 {
+                nums[left + left_offset + right_offset] = right_part[right_offset];
+                right_offset += 1;
+            } else {
+                nums[left + left_offset + right_offset] = right_part[right_offset];
+                right_offset += 1;
+                counter += (mid - left_offset + 1) as i32;
+            }
+        }
+        return counter;
     }
-    let mid = (low + high) >> 1;
 
-    if nums[mid].0 == target {
-        return mid;
+    fn _merge_sort(nums: &mut Vec<i32>, left: usize, right: usize) -> i32 {
+        let mut counter = 0;
+        if left + 1 < right {
+            let mid = (left + right) / 2;
+            counter += _merge_sort(nums, left, mid);
+            counter += _merge_sort(nums, mid, right);
+            counter += _merge(nums, left, mid, right);
+        }
+        return counter;
     }
-    if nums[mid].0 > target {
-        return if mid > 0 {
-            search(nums, target, low, mid - 1)
-        } else {
-            mid
-        };
-    }
-    if nums[mid].0 < target {
-        return search(nums, target, mid + 1, high);
-    }
-    return 0;
+    return _merge_sort(nums, 0, nums.len());
 }
 
 #[cfg(test)]
 mod test_offer_51 {
     use crate::Solution;
-    use std::collections::HashMap;
 
     #[test]
     fn test_case() {
-        let mut res = HashMap::new();
-        res.insert(1, 2);
-        res.insert(3, 4);
-        for (x, (key, value)) in res.iter().enumerate() {
-            println!("x: {} key {} value: {}", x, key, value);
-        }
-        // let nums = vec![];
-        // println!("{}", Solution::reverse_pairs(nums));
+        let nums = vec![7, 5, 6, 4];
+        println!("{}", Solution::reverse_pairs(nums));
     }
 }
